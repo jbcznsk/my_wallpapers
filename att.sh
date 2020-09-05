6bd68672d1373b1c8dab56d7a8272554d147b595
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2020-09-05 10:11:49 -0300

    0509

--- att.sh
+++ att.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ls |
+IMAGENS=$(ls | grep '.jpg\|.png' )
+
+echo "# Meus Wallpapers :)" > README.md
+
+for file in ${IMAGENS[@]};do 
+	echo '![](https://github.com/JorgeJabczenski/my_wallpapers/blob/master/'$file')' >> README.md; 
+done;
